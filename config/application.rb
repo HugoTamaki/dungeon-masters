17a6ee257e4bd722e713f8989be49b3bc839e1d3
Hugo Tamaki <hugokt2005@yahoo.com.br>
2014-06-14 08:44:19 -0300

    mudando js de grafo para colorir como cinza os nós e arestas

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,5 @@ module Calabouco
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
-    config.active_record.whitelist_attributes = false
   end
 end
