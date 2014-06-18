7cd5cf57ad21572b421d3774f82918f32186ad51
Hugo Tamaki <hugokt2005@yahoo.com.br>
2014-06-18 16:58:48 -0300

    usando cache para acelerar render de pagina de edição de história

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,7 +1,7 @@
 class Chapter < ActiveRecord::Base
 
   has_attached_file :image, styles: {thumbnail: "200x200>"}
-  belongs_to :story
+  belongs_to :story, touch: true
   has_many :decisions, dependent: :destroy
   has_many :monsters, dependent: :destroy
   has_many :modifiers_items, dependent: :destroy
