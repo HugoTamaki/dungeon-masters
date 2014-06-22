c99232ea25b3edef8dd59ddd7efd29639ec9a91c
Hugo Tamaki <hugokt2005@yahoo.com.br>
2014-06-22 08:57:14 -0300

    arrumando js e estilo de capitulos não usados

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,10 +3,12 @@
 # Supports bulk-adding hosts to roles, the primary server in each group
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
+set :stage, :production
+set :rails_env, :production
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{107.170.128.119}
+role :web, %w{107.170.128.119}
+role :db,  %w{107.170.128.119}
 
 
 # Extended Server Syntax
@@ -15,7 +17,7 @@ role :db,  %w{deploy@example.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server '107.170.128.119', user: 'root', roles: %w{web app db}, my_property: :my_value
 
 
 # Custom SSH Options
