1154e116880d22c1d83502853fa9251a5c1a43af
Hugo Tamaki <hugokt2005@yahoo.com.br>
2014-08-12 07:21:04 -0300

    resolvendo bug que faz não aparecer os destinos (erro chapter not found após apagar os capitulos)

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -3,7 +3,11 @@ class Decision < ActiveRecord::Base
   belongs_to :chapter
 
   def child
-    story = self.chapter.story
-    Chapter.by_story(story).find(self.destiny_num) unless self.destiny_num.nil?
+    begin
+      story = self.chapter.story
+      Chapter.by_story(story).find(self.destiny_num) unless self.destiny_num.nil?
+    rescue
+      nil
+    end
   end
 end
