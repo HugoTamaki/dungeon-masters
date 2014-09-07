a5e22d34edcfb0a7cfb96fa1b8e00a7634bdc1ad
Hugo Tamaki <hugokt2005@yahoo.com.br>
2014-09-07 06:49:35 -0300

    incluindo progress bar no lugar de atributos de aventureiro

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -15,6 +15,12 @@ $(document).ready(function(){
         $('#adventurer_skill').html(data["skill"]);
         $('#adventurer_energy').html(data["energy"]);
         $('#adventurer_luck').html(data["luck"]);
+        $('#adventurer_skill_bar').attr('aria-valuenow', data["skill"]);
+        $('#adventurer_energy_bar').attr('aria-valuenow', data["energy"]);
+        $('#adventurer_luck_bar').attr('aria-valuenow', data["luck"]);
+        $('#adventurer_skill_bar').css('width', (data["skill"] / 12.0) * 100 + '%');
+        $('#adventurer_energy_bar').css('width', (data["energy"] / 24.0) * 100 + '%');
+        $('#adventurer_luck_bar').css('width', (data["luck"] / 12.0) * 100 + '%');
         if (data["quantity"] > 0) {
           $('#' + data["name"] + '_item').next("#quantity").html(data["quantity"]);
         } else {
