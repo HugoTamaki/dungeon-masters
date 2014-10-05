3e90bad59c4141830e888ae8d2bc2326af0c9da0
Hugo Tamaki <hugokt2005@yahoo.com.br>
2014-10-05 12:58:42 -0300

    tentando nova configuração para enviar emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,8 +32,8 @@ Calabouco::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
-    authentication: "plain",
+    domain: "gmail.com",
+    authentication: :plain,
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
