841b0778a8f5670d00085a579145dd9b8a6ccc93
Hugo Tamaki <hugokt2005@yahoo.com.br>
2014-12-07 20:29:15 -0200

    corrigindo caminho após comentar em uma história

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,9 +4,9 @@ class CommentsController < ApplicationController
     @comment = @story.comments.new(comment_params)
     @comment.user_id = current_user.id
     if @comment.save
-      redirect_to story_prelude_path(@story, new_story: true), notice: "Comentário adicionado com sucesso."
+      redirect_to story_show_path(@story), notice: "Comentário adicionado com sucesso."
     else
-      redirect_to story_prelude_path(@story, new_story: true), alert: "Comentário não foi adicionado com sucesso."
+      redirect_to story_show_path(@story), alert: "Comentário não foi adicionado com sucesso."
     end
     
   end
@@ -15,7 +15,7 @@ class CommentsController < ApplicationController
     @story = Story.find(params[:story_id])
     @comment = @story.comments.find(params[:id])
     @comment.destroy
-    redirect_to story_prelude_path(@story, new_story: true), notice: "Comentário excluído."
+    redirect_to story_show_path(@story), notice: "Comentário excluído."
   end
 
   private
