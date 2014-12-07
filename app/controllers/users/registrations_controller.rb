41e881d7ffb53242c9910027e9628a8146e5a413
Hugo Tamaki <hugokt2005@yahoo.com.br>
2014-12-07 08:43:17 -0200

    inserindo about_me, melhorando forms e permitindo update de about_me

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,10 +2,38 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+
+    # custom logic
+    if params[:user][:password].present?
+      result = resource.update_with_password(params[resource_name])
+    else
+      result = resource.update_without_password(params[resource_name].permit(:about_me))
+    end
+
+    # standart devise behaviour
+    if result
+      if is_navigational_format?
+        if resource.respond_to?(:pending_reconfirmation?) && resource.pending_reconfirmation?
+          flash_key = :update_needs_confirmation
+        end
+        set_flash_message :notice, flash_key || :updated
+      end
+      sign_in resource_name, resource, :bypass => true
+      respond_with resource, :location => after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
+
   protected
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) << :name
+      devise_parameter_sanitizer.for(:sign_up) << :about_me
       devise_parameter_sanitizer.for(:account_update) << :name
+      devise_parameter_sanitizer.for(:account_update) << :about_me
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141206203810) do
+ActiveRecord::Schema.define(version: 20141207095346) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20141206203810) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.text     "about_me"
     t.index ["email"], :name => "index_users_on_email", :unique => true
     t.index ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
   end
