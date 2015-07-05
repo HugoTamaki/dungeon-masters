f98069c662c271526c14bce3a4371d216562dfb8
Hugo Tamaki <hugokt2005@yahoo.com.br>
2015-07-05 14:01:41 -0300

    fixing spec database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -48,6 +48,11 @@ RSpec.configure do |config|
   Warden.test_mode!
 
   config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
   config.before(:each) do
     DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :transaction
     DatabaseCleaner.start
