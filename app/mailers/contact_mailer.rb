e8adeed13e4a37b1a53a72c0326ae458d48f83dd
Hugo Tamaki <hugokt2005@yahoo.com.br>
2015-09-25 17:53:44 -0300

    adding specs

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,6 +4,6 @@ class ContactMailer < ActionMailer::Base
   def contact_email(params)
     @from_email = params[:email]
     @message = params[:message]
-    mail(from: @from_email, to: 'hugotamaki@gmail.com', subject: 'Email de contato - Dungeon Masters')
+    mail(from: @from_email, to: ENV["mail_to"], subject: 'Email de contato - Dungeon Masters')
   end
 end
--- spec/mailers/contact_mailer_spec.rb
+++ spec/mailers/contact_mailer_spec.rb
@@ -1,5 +1,21 @@
 require "spec_helper"
 
 describe ContactMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#contact_email' do
+    let(:params) { {email: 'teste@mailinator.com', message: 'this is a message'} }
+    let(:mail) { ContactMailer.contact_email(params) }
+
+    it 'should have correct from' do
+      expect(mail.from).to eql(['teste@mailinator.com'])
+    end
+
+    it "should set right subject" do
+      expect(mail.subject).to eq("Email de contato - Dungeon Masters")
+    end
+
+    it 'should show informations on the email body' do
+      expect(mail.body.encoded).to match(/#{params[:email]}/)
+      expect(mail.body.encoded).to match(/#{params[:message]}/)
+    end
+  end
 end
